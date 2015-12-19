183a74d601d63d5edfa7a774545eb6432056c799
dragonslayer95 <dragonslayerx@outlook.com>
2015-12-19 17:28:09 +0530

    added: Session.handlePush and  Session.handleGet

--- newDocHandler.js
+++ newDocHandler.js
@@ -1,10 +1,10 @@
-
-
 var DEBUG = true;
+
 /**
- *
- *
- *
+ * State is a class which maintains info about current server-state of Doc#docId and edits made during the session.
+ * @constructor
+ * @param docId {String} docId of Doc, to be edited
+ * @param docPath {String} Path of Doc#docId on server
  */
 function State (docId, docPath) {
 	this.docId = docId;
@@ -41,46 +41,43 @@ function State (docId, docPath) {
 }
 
 /**
- * Returns the synStamp of current-state of server`
- *
+ * @return {Number} Returns the synStamp of current-state of server`
  */
 State.prototype.getSynStamp = function() {
 	return this.transformedOperations.length; 
 }
 
-
 /**
- * Returns the current-state of server`
- *
+ * @return {Number} Returns the current-state of server`
  */
 State.prototype.getState = function() {
 	return this.docState.toString();
 }
 
 /**
- *
- *
- *
+ * Session is a wrapper class which maintains the state, info about active users, their respective state-pulling time-stamps and cursor-positions  
+ * @constructor
+ * @param docId {String} docId of Doc, to be edited
+ * @param docPath {String} Path of Doc#docId
  */
-function Session(docId) {
-	this.state = new State(docId);
+function Session(docId, docPath) {
+	this.state = new State(docId, docPath);
 	this.userCursorPos = {};
 	this.userSynTime = {};
 };
 
 /**
  * Adds the user to the session
- *
  * @param {String} userId - User's userId to be added to this session
  */
 Session.prototype.addUser = function(userId) {
 	userSynTime[userId] = this.session.getSynStamp();
+	userCursorPos[userId] = 0;
 };
 
 
 /**
  * Removes the user from the session
- *
  * @param {String} userId - User's userId to be removed from this session
  */
 Session.prototype.removeUser() = function(userId) {
@@ -100,80 +97,111 @@ Session.prototype.removeUser() = function(userId) {
 }
 
 /**
- * Handles push request of session with Doc#docId
+ * Handles PUSH request of session with Doc#docId
  *
- * @param {String} userId - User's userId who edited the document
+ * @param request {Object} User's request object
+ * @param request.body {Array} Edit-operations pushed by the user.
+ * @param response {Object} User's response object
+ * @param userId {String} User's userId who edited the document
  */
-Session.prototype.handlePush() = function(request, response, userId) {
-	try {
-		//state of Doc#docId
-		var state = this.state; 
+Session.prototype.handlePush = function(request, response, userId) {
+	//state of Doc#docId
+	var state = this.state; 
+	
+	//bulk PUSH request received from User#userId
+	var operationsRecvd = request.body;
+	
+	operationsRecvd.foreach( function(operation) {
 		
-		//bulk PUSH request received from User#userId
-		var operationsRecvd = request.body;
+		//transform the operation
+		var transformed = operationalTransform.transform(operation, this.localOperations);
 		
-		operationsRecvd.foreach( function(operation) {
+		//set the synTimeStamp of the transformed
+		var synTimeStamp = state.getSynStamp(); 
+		transformed.timeStamp = synTimeStamp;
+		
+		//perform the necessary editing 
+		if (transformed.type == 'REPOSITION') {
+			this.userCursorPos[userId] = transformed.position;
+		} else {	
+			//update transformedOperations to notify users of state-changes 
+			this.state.transformedOperations.push(transformed);
 			
-			//transform the operation
-			var transformed = operationalTransform.transform(operation, this.localOperations);
-			
-			//set the synTimeStamp of the transformed
-			var synTimeStamp = state.getSynStamp(); 
-			transformed.timeStamp = synTimeStamp;
-			
-			//perform the necessary editing 
-			if (transformed.type == 'REPOSITION') {
-				this.userCursorPos[userId] = transformedOp.position;
-			} else {
-				//logs the received operation
-				if (DEBUG) {
-					console.log('PUSH Received : ');
-					console.log(userId);
-					console.log(operation);
-				}
+			//clone and push the operation for transformation
+			var cloned = Object.assign({}, transformed);
+			this.state.localOperations.push(cloned);
 
-				transformedOperations[docId].push(transformedOp);
+			//update the server state of the doc
+			this.state.applyToRope(transformedOp);
+			this.state.operationsNotSaved.push(transformed);
 
-				var obj = JSON.parse(JSON.stringify(transformedOp));
-				localOperations[docId].push(obj);
+			log('PUSH', operation);
+			log('TRANSFORMED', transformed);
+			log('STATE', this.state.getState());
+		}
+	});
+	
+	response.end();
+}
 
-				//Update the server state of the doc
-				applyToRope(docId, transformedOp);
-				operationsNotSaved[docId].push(transformedOp);
-
-				//logs the transformed operation
-				if (DEBUG) {
-					console.log('TRANSFORMED Received : ');
-					console.log(userId);
-					console.log(transformedOp);
-				}
-				//Print server state
-				if (DEBUG) {
-					console.log('STATE: ');
-					console.log(docState[docId].toString());
-				}
-			}
-		});
+/**
+ * Handles GET request of session with Doc#docId
+ *
+ * @param request {Object} User's request object
+ * @param response {Object} User's response object
+ * @param userId {String} User's userId who wants to pull the state
+ */
+Session.prototype.handleGet = function(request, response, userId) {
+	
+	var getOperation = request.body;
+	var prevTimeStamp = this.userSynTime[userId];
+	var currentTimeStamp = 0; 
+	
+	//editing done by other users since the users last pulled state from server
+	var changesToSync = [];
+	var size = this.state.transformedOperations.length;
+	
+	const GET_THRESHHOLD = 10;
+	for (var i = prevTimeStamp; i < size && changesToSync.length < GET_THRESHHOLD; i++) {
+		var operation = this.state.transformedOperations[i];
+		changesToSync.push(operation);
+		currentTimeStamp = operation.timeStamp + 1;
+		count++;
+	}
+	
+	if (changesToSync.length > 0) {
+		//send changes not yet synced
+		this.synTimeStamps[userId] = currentTimeStamp;
+		response.json(changesToSync);
 		response.end();
-		
-	} catch (err) {
-		console.log(err.msg);
+	} else {
+		//if no operations to be pushed, update the client about client position on doc
+		var repositionOperations = [];
+		for (var user in this.userCursorPos) {
+			repositionOperations.push(
+				{
+					userId: user,
+					synTimeStamp: this.state.getSynStamp(), 
+					position: this.userCursorPos[user]
+				}
+			);
+		}
+		response.json(repositionOperations[docId]);
+		response.end();
 	}
 }
 
 /**
- *
- *
- *
+ * SessionManager is a class whose objective is to create, maintain sessions. It also route user-requests to appropriate sessions    
+ * @constructor
  */
-var SessionManager = function() {
+ var SessionManager = function() {
 	this.sessions = {};
 }
 
 /**
  * Creates a new session for Doc#docId, if none exists. Else uses an existing session. Adds the User#userId to the session. 
- * Returns the current state of the doc#docID.
- *
+ * @return {String} current state of the doc#docID.
  * @param {String} userId - User's userId who has sent REGISTER request.
  * @param {String} docId - Doc's docId which user wants to edit.
  * @param {String} docPath - Path of the doc on server.
@@ -221,8 +249,10 @@ var SessionManager.prototype.handleUnregister (userId, docId) {
 }
 
 /**
- * Checks for validity of GET request. Forwards request to session Doc#docId, if one exists.
+ * Checks for validity of requests. Forwards requests to session Doc#docId, if one exists.
  *
+ * @param {Object} request  - User's request object
+ * @param {Object} response - User's response object
  * @param {String} userId - User's userId who has sent PUSH request.
  * @param {String} docId - Doc's docId which user wants to edit.
  */
@@ -230,44 +260,16 @@ var SessionManager.prototype.handleUnregister (userId, docId) {
 	try {
 		if (this.session[docId] == undefined) {
 			throw {
-				msg: 'get request for non-existent doc#' + docId;
+				msg: 'request for non-existent doc#' + docId;
 			};
 		} else {
 			var session = this.sessions[docId];
 			if (session.state.userCursorPos[userId] == undefined) {
 				throw {
-					msg: 'get request for non-existent user#' + userId + ' on doc#' + docId;
+					msg: 'request for non-existent user#' + userId + ' on doc#' + docId;
 				};
 			} else {
-				session.handleGet(request, response, userId);
-			}
-		}
-	} catch(err) {
-		console.log(err.msg)
-	}
-}
-
-
-/**
- * Checks for validity of PUSH request. Forwards request to session Doc#docId, if one exists.
- *
- * @param {String} userId - User's userId who has sent PUSH request.
- * @param {String} docId - Doc's docId which user wants to edit.
- */
-var SessionManager.prototype.handlePush (request, response, userId, docId) {
-	try {
-		if (this.session[docId] == undefined) {
-			throw {
-				msg: 'push request for non-existent doc#' + docId;
-			};
-		} else {
-			var session = this.sessions[docId];
-			if (session.state.userCursorPos[userId] == undefined) {
-				throw {
-					msg: 'p request for non-existent user#' + userId + ' on doc#' + docId;
-				};
-			} else {
-				session.handlePush(request, response, userId);
+				//routing table
 			}
 		}
 	} catch(err) {
